a691acb59ee7539b0e948fb3e098f98dcf6cb4c7
Justin Yun <justinyun@google.com>
2020-07-27 17:20:08 +0900

    vayu: Enforce product interfaces
    Add 'PRODUCT_PRODUCT_VNDK_VERSION := current' and
    'PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true' to enforce
    product interfaces
    
    Change-Id: I25436ecc8b4f2e90ed54b46fd976c66d897348db

--- device.mk
+++ device.mk
@@ -22,6 +22,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Enforce native interfaces of product partition as VNDK
+PRODUCT_PRODUCT_VNDK_VERSION := current
+
+# Enforce java interfaces of product partition
+PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
+
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
