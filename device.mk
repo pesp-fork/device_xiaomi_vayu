b4ccacc9d6655ed009efafeb099a657ad3c5e12f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:36:57 +0100

    vayu: Setup namespace
    Change-Id: I97172190de6bcf572021b5a252a1dc587e1a3fc4

--- device.mk
+++ device.mk
@@ -10,5 +10,12 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/vayu/vayu-vendor.mk)
