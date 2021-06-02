ec057fa625e3c932bd9bd0024cb70b6efa79f122
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-02 17:58:37 +0200

    vayu: Build LiveDisplay HIDL
    Change-Id: I6b767fe2269b679438c8fc0c49ceef9110a96a23

--- device.mk
+++ device.mk
@@ -231,6 +231,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.msmnile
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
