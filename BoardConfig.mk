550d4680844aea48f8d05cc43861b8742a954776
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-28 21:29:43 +0200

    vayu: Add a minimal framework compatibility matrix
    * Made with https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324
    
    Change-Id: I180d19e7939ee0c3e9f1aeb04a854f8aaf444235

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
