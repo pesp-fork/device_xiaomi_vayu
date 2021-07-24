1093ee303b204eacc053550b7174948fafcfe1a1
Lunarixus <Nathan@lunarixus.dev>
2021-07-24 17:00:35 +0800

    vayu: configs:  Add Pixel 2016 exclusive xml * This allows us to use unlimited backup in Google Photos infinitely.
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Change-Id: I461b6057baddb090ca94e838bca9d13a76d15fc8
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Signed-off-by: officialputuid <officialputuid@hack.id>
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- device.mk
+++ device.mk
@@ -228,6 +228,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msmnile
