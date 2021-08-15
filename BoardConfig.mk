e094c6c6e425ebebc80dd285e20b4ae6b5ea6fd3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-15 02:58:51 +0200

    vayu: Update blobs to vayu V12.5.3.0.RJUMIXM
    Change-Id: Ib1497889c57baca09f7b966cfa2cc9a50126d864

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-05-01
+VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
