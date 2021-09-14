aa6aa65675a1959a89406f5872c7d5bc054d61d8
SebaUbuntu <barezzisebastiano@gmail.com>
2021-09-14 06:06:16 +0000

    vayu: Initial skeleton

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/vayu
+
+# Inherit the proprietary files
+include vendor/xiaomi/vayu/BoardConfigVendor.mk
